71d0f57a53819063b6837f007f16f3d8ed002e33
StephenStrickland <sstrick5@spsu.edu>
2015-07-02 13:43:53 -0400

    Changes Json.Net version to 6.0.6

--- GcmSharp/GcmSharp/Properties/AssemblyInfo.cs
+++ GcmSharp/GcmSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GcmSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a Google Cloud Messaging helper for C#. Currently only messaging is supported, I am currently working on adding functionality to manage device groups and use Google's Instance-Id system.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GcmSharp")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
