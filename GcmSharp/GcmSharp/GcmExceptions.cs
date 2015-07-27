04bbe288acdfb11b1818bfbb8410fb9f03cf5fe4
StephenStrickland <sstrick5@spsu.edu>
2015-07-27 10:16:11 -0400

    Adds exception classes.

---
+++ GcmSharp/GcmSharp/GcmExceptions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcmSharp
+{
+    class GcmManagerTimeoutException : Exception
+    {
+        public GcmManagerTimeoutException()
+            : base() { }
+
+        public GcmManagerTimeoutException(string message, GcmMessage gcmMessage)
+            : base(message) { Message = gcmMessage; }
+
+        public GcmManagerTimeoutException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public GcmManagerTimeoutException(string message, Exception innerException, GcmMessage gcmMessage)
+            : base(message, innerException) { Message = gcmMessage; }
+
+        public GcmManagerTimeoutException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+
+        protected GcmManagerTimeoutException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        public GcmMessage Message { get; set; }
+    }
+
+    public class GcmException : Exception
+    {
+        public GcmException()
+            : base() { }
+
+        public GcmException(string message, GcmMessage gcmMessage)
+            : base(message) { Message = gcmMessage; }
+
+        public GcmException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public GcmException(string message, Exception innerException, GcmMessage gcmMessage)
+            : base(message, innerException) { Message = gcmMessage; }
+
+        public GcmException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+
+        protected GcmException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        public GcmMessage Message { get; set; }
+    }
+}
--- GcmSharp/GcmTest/Program.cs
+++ GcmSharp/GcmTest/Program.cs
@@ -19,16 +19,18 @@ namespace GcmTest
             GcmManagerOptions options = new GcmManagerOptions() { 
                 AuthenticationKey = AuthKey, 
                 PackageName = PackageName,
-                SenderId = SenderId
+                SenderId = SenderId,
+                Expect100Continue = false, //default
+                UseNagleAlgorithm = false //default
             };
+
             GcmManager manager = new GcmManager(options);
 
-
-
-            GcmMessage message = new GcmMessage().To("APA91bEVUAdCF4XdfV6cnecuih5SY48lePF8ZCiBgL8fgqRlB9CKlRyZbqOpfOOfP3SkOG2VYH1LDrYi80aotHg1at6QdcDrk_2t1_Iopw7MAkjv0dD-4DCbyqw7qKpF7INfiRQ2u_Ax")
-                .WithRestrictedPackageName(PackageName).WithTimeToLive(400)
-                .WithData(new { body = "Client: MR. SMITH, Appointment Time: today 3:00pm", title = "Your Appointment Has Arrived" });
-               // .WithNotification(new GcmNotification().WithBody("body text here").WithTitle("Title Here"));
+            GcmMessage message = new GcmMessage().To("RegistrationTokenHere")
+                .WithData(new { body = "Client: MR. SMITH, Appointment Time: today 3:00pm", title = "Your Appointment Has Arrived" })
+                .WithNotification(new GcmNotification().WithBody("body text here").WithTitle("Title Here"));
+               // ;
+                //.WithRestrictedPackageName(PackageName).WithTimeToLive(400)
                 //.WithIcon("myIcon.png").WithColor("color"));
 
 
@@ -39,7 +41,7 @@ namespace GcmTest
                 watch.Start();
                 Console.WriteLine("getting instance response");
 
-                var response = manager.SendMessage(message);
+                GcmMessageResponse response = manager.SendMessage(message);
                 //var r = manager.GetInstanceIdResponse(true, "APA91bEVUAdCF4XdfV6cnecuih5SY48lePF8ZCiBgL8fgqRlB9CKlRyZbqOpfOOfP3SkOG2VYH1LDrYi80aotHg1at6QdcDrk_2t1_Iopw7MAkjv0dD-4DCbyqw7qKpF7INfiRQ2u_Ax");
                 //Console.WriteLine(r.ToString());
 
